ce837b7de62e7e8211fe351778c260e22ad13397
Bettina Berec <berec.bettina@gmail.com>
2026-03-06 21:21:21 +0100

    Alap3

---
+++ admin_frontend/script.js
@@ -0,0 +1,20 @@
+const loginForm = document.getElementById("loginForm");
+
+if (loginForm) {
+  loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const errorMessage = document.getElementById("errorMessage");
+
+    errorMessage.textContent = "";
+
+    // ideiglenes fix login
+    if (email === "admin@escape.hu" && password === "admin123") {
+      window.location.href = "dashboard.html";
+    } else {
+      errorMessage.textContent = "Hibás email vagy jelszó.";
+    }
+  });
+}
